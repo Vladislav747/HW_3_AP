7e7bcfd64325e5a2e85d33f62359744a1094c516
Владислав Кудряков <vvkudriakov@edu.hse.ru>
2025-04-01 22:41:51 +0500

    Added load_testing

--- backend/tests/load_testing/locustfile.py
+++ backend/tests/load_testing/locustfile.py
@@ -2,6 +2,7 @@ from locust import HttpUser, task, between
 from faker import Faker
 
 class ShortenerUser(HttpUser):
+    host = "http://localhost:8000"
     wait_time = between(0.5, 2)
     fake = Faker()
 
